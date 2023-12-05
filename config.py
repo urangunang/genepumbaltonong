7d627f673641b19911248260468c1a6aa6827f2e
urangunang <145663184+urangunang@users.noreply.github.com>
2023-12-05 10:03:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,9 @@ load_dotenv("config.env")
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 # API ID Anda dari my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = os.environ.get("APP_ID", "")
+if APP_ID:
+    APP_ID = int(APP_ID)
 
 # API Hash Anda dari my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
